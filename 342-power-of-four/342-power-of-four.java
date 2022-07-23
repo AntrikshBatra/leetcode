bbdaf1a2cedfd4c97603856a131c78aea1c6694a
Antriksh Batra <67945400+AntrikshBatra@users.noreply.github.com>
2022-07-23 18:29:58 +0530

    Time: 1 ms (100.00%), Space: 39.6 MB (90.47%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0)
+        {
+            return false;
+        }else
+        {
+            while(n%4==0)
+            {
+                n=n/4;
+            }
+        }
+        return n==1;
+    }
+}
