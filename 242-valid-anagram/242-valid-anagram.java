1b052a3f9d6822cda3167ae5cece96101e1631fe
Antriksh Batra <67945400+AntrikshBatra@users.noreply.github.com>
2022-08-01 02:01:00 +0530

    Time: 5 ms (73.11%), Space: 43.9 MB (54.36%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i:count)
+        {
+            if(i!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
